f5f44c835b5095ba9baddf5ffaa36152c3383ba3
iiishop <18958677677@163.com>
2025-01-04 05:18:59 +0800

    合集创建

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 import PostPanel from './components/PostPanel.vue';
 import SelfIntroduction from './components/SelfIntroduction.vue';
+import Collection from './components/Collection.vue';
 
 // 数据数组，每个对象包含 imageUrl 和 markdownUrl
 const posts = ref([
@@ -21,16 +22,22 @@ const posts = ref([
     </div>
     <div class="RightList">
       <SelfIntroduction />
+      <div class="CollectionPanel">
+        <h1>Collections</h1>
+        <Collection :name="'旮旯给'" :imageUrl="'/src/Posts/Images/1.jpg'" :createDate="'2023-10-10'" :count="10" />
+      </div>
     </div>
   </div>
 </template>
 
 <style>
-.Scene {
+body{
   margin: 0;
   padding: 0;
   font-family: 'Noto Sans SC', sans-serif;
   background: white;
+}
+.Scene {
   display: flex;
   flex-direction: row;
   width: 100vw;
@@ -46,5 +53,25 @@ const posts = ref([
   align-items: center;
   justify-content: center;
   width: 50vw;
+}
+
+.RightList {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+  justify-content: center;
+  width: 25rem;
+}
+.CollectionPanel {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  color: black;
+  padding: 1rem;
+  width: 100%;
+  border-radius: 20px;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
+  height: auto;
 }
 </style>
---
+++ src/components/Collection.vue
@@ -0,0 +1,56 @@
+<script setup>
+const props = defineProps({
+    name: String,
+    imageUrl: String,
+    createDate: String,
+    count: Number,
+});
+</script>
+
+<template>
+    <div class="collection">
+        <img :src="imageUrl" alt="Collection" />
+        <div class="info">
+            <h2>{{ name }}</h2>
+            <p>{{ createDate }}</p>
+            <p>内有{{ count }}篇文章</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.collection {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    width: 100%;
+    padding: 1rem;
+    background-color: #f7f7f7;
+    border-radius: 15px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+}
+
+.info {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+}
+
+img {
+    width: 8rem;
+    height: 5rem;
+    object-fit: cover;
+    border-radius: 10px;
+    transition: transform 0.2s ease;
+}
+
+p {
+    color: #666;
+    margin: 0.5rem 0 0;
+}
+
+.collection:hover img {
+    transform: scale(1.05);
+}
+</style>
