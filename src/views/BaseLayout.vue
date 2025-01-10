b25a4dee0c0e386cd4fc1ce9f96ebf842cd83d13
iiishop <18958677677@163.com>
2025-01-11 02:53:56 +0800

    创建BaseLayout以减少冗余

---
+++ src/views/BaseLayout.vue
@@ -0,0 +1,70 @@
+<!-- BaseLayout.vue -->
+<template>
+  <div>
+    <HeadMenu />
+    <div class="Scene">
+      <div class="LeftList">
+        <slot name="left"></slot>
+      </div>
+      <div class="MidList">
+        <slot name="middle"></slot>
+      </div>
+      <div class="RightList">
+        <SelfIntroduction />
+        <CollectionPanel />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import HeadMenu from '@/components/HeadMenu.vue';
+import SelfIntroduction from '@/components/SelfIntroduction.vue';
+import CollectionPanel from '@/components/CollectionPanel.vue';
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Noto Sans SC', sans-serif;
+  background: var(--body-background-color);
+  color: var(--body-text-color);
+  overflow: auto;
+}
+
+.Scene {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 2rem;
+  padding: 2rem;
+  padding-top: 6rem;
+}
+
+.RightList {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+  width: 25rem;
+  color: var(--rightlist-text-color);
+}
+
+.MidList {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+  max-width: calc(100% - 45rem - 8rem);
+  min-width: 30rem;
+}
+
+.LeftList {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+  max-width: 20rem;
+}
+</style>
--- src/views/TagView.vue
+++ src/views/TagView.vue
@@ -1,69 +1,12 @@
 <script setup>
-import SelfIntroduction from '@/components/SelfIntroduction.vue';
-import HeadMenu from '@/components/HeadMenu.vue';
+import BaseLayout from '@/views/BaseLayout.vue';
 import TagPanel from '@/components/TagPanel.vue';
-import CollectionPanel from '@/components/CollectionPanel.vue';
-
 </script>
 
 <template>
-  <HeadMenu />
-  <div class="Scene">
-    <div class="LeftList"></div>
-    <div class="MidList">
+  <BaseLayout>
+    <template #middle>
       <TagPanel />
-    </div>
-    <div class="RightList">
-      <SelfIntroduction />
-      <CollectionPanel />
-    </div>
-  </div>
+    </template>
+  </BaseLayout>
 </template>
-
-<style>
-body {
-  margin: 0;
-  padding: 0;
-  font-family: 'Noto Sans SC', sans-serif;
-  background: var(--body-background-color);
-  color: var(--body-text-color);
-  overflow: auto;
-}
-
-.Scene {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  gap: 2rem;
-  padding: 2rem;
-  padding-top: 6rem;
-}
-
-.RightList {
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-  align-items: center;
-  width: 25rem;
-  color: var(--rightlist-text-color);
-}
-
-.MidList {
-
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-  align-items: center;
-  max-width: calc(100% - 45rem - 8rem);
-  min-width: 30rem;
-}
-
-.LeftList {
-
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-  align-items: center;
-  max-width: 20rem;
-}
-</style>
